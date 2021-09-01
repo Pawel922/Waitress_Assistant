b118c22ebd961be5b19111ed9438a09bf75175aa
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-09-01 21:25:17 +0200

    Basic structure was implemented

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './Header';
+import Menu from './Menu';
+import Summary from './Summary';
+import Tables from './Tables';
+import Footer from './Footer';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Waitress_Assistant
-      </div>
+      <Router>
+        <div className="app">
+          <header>
+            {<Header/>}
+          </header>
+          <main>
+            <aside>
+              <div className="menu">
+                {<Menu/>}
+              </div>
+              <div className="summary">
+                {<Summary/>}
+              </div>
+            </aside>
+            <section className="tables">
+              {<Tables/>}
+            </section>
+          </main>
+          <footer>
+            {<Footer/>}
+          </footer>
+        </div>
+      </Router>
     );
   }
 }

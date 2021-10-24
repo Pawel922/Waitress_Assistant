50b70cc1eb6b30b7cd5b61120472367806f9625d
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2021-10-24 18:25:32 +0200

    Updates

---
+++ precache-manifest.d10410fc614c012083e8604279138501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccb93bcc0904872d827d",
+    "url": "/Waitress_Assistant/static/css/main.edba22a2.chunk.css"
+  },
+  {
+    "revision": "ccb93bcc0904872d827d",
+    "url": "/Waitress_Assistant/static/js/main.bbb15f51.chunk.js"
+  },
+  {
+    "revision": "9d1d66734186b5e864d5",
+    "url": "/Waitress_Assistant/static/js/runtime~main.de8597f6.js"
+  },
+  {
+    "revision": "51faa0482f3325b5f2ae",
+    "url": "/Waitress_Assistant/static/js/2.d3051d68.chunk.js"
+  },
+  {
+    "revision": "0ec3937684bcae76102afa916c3914ea",
+    "url": "/Waitress_Assistant/static/media/background.0ec39376.jpg"
+  },
+  {
+    "revision": "e8eb29bffc541d3bd8aad7e97714e61e",
+    "url": "/Waitress_Assistant/static/media/table.e8eb29bf.jpg"
+  },
+  {
+    "revision": "737d36704abc86410dbd4037dd290667",
+    "url": "/Waitress_Assistant/index.html"
+  }
+];
